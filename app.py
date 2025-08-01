685e7b20b06efb409029598ce6a0ee42cbb2c67c
JahirWH <93404854+JahirWH@users.noreply.github.com>
2025-08-01 00:25:52 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+
+def main():
+
+    comand = "ppython3 -m http.server 8000"
+    print("Up and running on http://localhost:8000")
+
+    os.system(comand)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
