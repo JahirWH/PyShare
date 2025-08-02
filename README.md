450fa91300a508e6df3866c522f544daf1721848
JahirWH <93404854+JahirWH@users.noreply.github.com>
2025-08-01 20:34:43 -0600

    ip

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # PyShare
+### Share files local wifi linux to iphone
+
+## How use?
+
+- just run app.py 
+```bash
+python3 app.py
+```
+- and , open safary with the url from app.py show in console, example:
+
+- 
--- app.py
+++ app.py
@@ -1,13 +1,35 @@
 import os
+import socket
+
+
+
+def obtener_ip_local():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))  
+        ip_local = s.getsockname()[0]
+        s.close()
+        return ip_local
+    except Exception as e:
+        return f"Error: {e}"
 
 def main():
+    try:
+        import http.server
+        comand = "python3 -m http.server 8730"
+        os.system(comand)
+  
+        print("To stop the server, press Ctrl+C")
+    except ImportError:
+        print("Error: http.server module not found. Please ensure you have Python installed.")
+        return
 
-    comand = "ppython3 -m http.server 8000"
-    print("Up and running on http://localhost:8000")
 
-    os.system(comand)
 
 if __name__ == "__main__":
+    obtener_ip_local()
+    print("Up and running on http://localhost:8730")
+    print("Up and running on http://" + obtener_ip_local() + ":8730")
     main()
 
 
