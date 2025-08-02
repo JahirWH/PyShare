50dd039cc343431e70cad60768c5b4e44b2ab6d6
JahirWH <93404854+JahirWH@users.noreply.github.com>
2025-08-01 20:54:24 -0600

    index,json

--- app.py
+++ app.py
@@ -1,7 +1,52 @@
 import os
 import socket
+import http.server
+import socketserver
+import json 
+
+PORT = 8730
+
+class MiServidor(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/api/files':
+            self.listar_archivos()
+        else:
+            super().do_GET()  
+    def listar_archivos(self):
+        archivos = []
+
+        for nombre_archivo in os.listdir('.'):
+            if os.path.isfile(nombre_archivo):
+                archivos.append({
+                    "name": nombre_archivo,
+                    "size": os.path.getsize(nombre_archivo),
+                    "type": "file"
+                })
+
+        respuesta = json.dumps(archivos).encode('utf-8')
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Content-Length', str(len(respuesta)))
+        self.end_headers()
+        self.wfile.write(respuesta)
 
 
+
+def main():
+    try:
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(script_dir)
+
+        with socketserver.TCPServer(("", PORT), MiServidor) as httpd:
+            ip_local = obtener_ip_local()
+            print(f"\n Server online in:")
+            print(f" http://localhost:{PORT}")
+            print(f" http://{ip_local}:{PORT}")
+            print("  Ctrl+C for stop.\n")
+            httpd.serve_forever()
+
+    except KeyboardInterrupt:
+        print("\n Server stoped.")
 
 def obtener_ip_local():
     try:
@@ -13,25 +58,8 @@ def obtener_ip_local():
     except Exception as e:
         return f"Error: {e}"
 
-def main():
-    try:
-        import http.server
-        comand = "python3 -m http.server 8730"
-        os.system(comand)
-  
-        print("To stop the server, press Ctrl+C")
-    except ImportError:
-        print("Error: http.server module not found. Please ensure you have Python installed.")
-        return
-
-
-
 if __name__ == "__main__":
-    obtener_ip_local()
-    print("Up and running on http://localhost:8730")
-    print("Up and running on http://" + obtener_ip_local() + ":8730")
     main()
-
 
 
 
